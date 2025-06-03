e2e5e4201f1f8556bc5bd4dab0d4107905ee869a
Khant Thu Aung <wannathu9@gmail.com>
2025-06-03 15:32:33 +0800

    iteration5 + small changes

---
+++ iteration5/IdentifiableObject/LookCommand.cs
@@ -0,0 +1,58 @@
+namespace SwinAdventure;
+
+public class LookCommand : Command
+{
+
+    public LookCommand() : base(new string[] { "look" }) { }
+    public override string Execute(Player p, string[] text)
+    {
+        if (text[0].ToLower() != "look")
+        {
+            return "Error in look input";
+        }
+        if (text.Length != 3 && text.Length != 5)
+        {
+            return "I don't know how to look like that";
+        }
+        if (text[1].ToLower() != "at")
+        {
+            return "What do you want to look at?";
+        }
+        if (text.Length == 5 && text[3].ToLower() != "in")
+        {
+            return "What do you want to look in?";
+        }
+        string itemId = text[2];
+        IHaveInventory container;
+
+        if (text.Length == 3)
+        {
+            container = p;
+        }
+        else
+        {
+            string containerId = text[4];
+            container = FetchContainer(p, containerId);
+            if (container == null)
+            {
+                return $"I can't find the {containerId}";
+            }
+        }
+        return LookAtIn(itemId, container);   
+    }
+
+    private IHaveInventory FetchContainer(Player p, string containerId)
+    {
+        GameObject obj = p.Locate(containerId);
+        return obj as IHaveInventory;
+    }
+    private string LookAtIn(string thingId, IHaveInventory container)
+    {
+        GameObject item = container.Locate(thingId);
+        if (item == null)
+        {
+            return $"I can't find the {thingId}";
+        }
+        return item.FullDescription;
+    }
+}
---
+++ iteration5/IdentifiableObject/Program.cs
@@ -0,0 +1,56 @@
+﻿namespace SwinAdventure;
+
+public class Program
+{
+    public static void Main(string[] args)
+    {
+        string playerName;
+        string playerDescription;
+
+        Console.WriteLine("Enter your name, Adventurer!");
+        Console.Write("Name -> ");
+        playerName = Console.ReadLine();
+        Console.WriteLine("Now how do you describe yourself, Adventurer!");
+        Console.Write("Description -> ");
+        playerDescription = Console.ReadLine();
+
+        Player me = new(playerName, playerDescription);
+
+        Item shield = new(new string[] { "shield", "broken shield" }, "A broken shield", "A sheild that's been through alot");
+        Item sword = new(new string[] { "sword", "dull sword" }, "A dull sword", "A sword that's been fought for its life");
+
+        me.Inventory.Put(shield);
+        me.Inventory.Put(sword);
+        Bag bag = new(new string[] { "bag", "brown bag" }, "A brown bag", "A small brown bag already have two holes");
+        me.Inventory.Put(bag);
+        Item ring = new(new string[] { "ring", "gold ring" }, "A gold ring", "A high value ring that's been passed down to you");
+        bag.Inventory.Put(ring);
+
+        while (true)
+        {
+            string command = "";
+            Console.Write("Command -> ");
+            command = Console.ReadLine();
+
+            switch (command)
+            {
+                case "exit":
+                    {
+                        Console.WriteLine("Bye Adventurer!");
+                        return;
+                    }
+                case "end":
+                    {
+                        Console.WriteLine("Bye Adventurer!");
+                        return;
+                    }
+                default:
+                    {
+                        Console.WriteLine(new LookCommand().Execute(me, command.Split(' ')));
+                        break;
+                    }
+            }
+        }
+    }
+
+}
